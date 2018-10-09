1777a579496a3a2f47d4c91505d156a6f5565502
yuwei <463313681@qq.com>
2018-10-09 22:16:09 +0800

    格式化代码

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -13,56 +13,37 @@ use Maatwebsite\Excel\Facades\Excel;
 class IndexController extends Controller
 {
 
-
-    //
-
-    public function test(Request $request)
-    {
-//        $keys = $request->keys();
-//        $res = \DB::table('chengyu')->select()->selectRaw('view_count + ? as view_new',[99])->limit(5)->get();
-        $res = \DB::table('chengyu')->select(\DB::raw('view_count + 99 as view_new'))->limit(5)->get();
-        dd($res);
-//        $res = \DB::table('chengyu')->forPage(2)->get();
-//        $res = \DB::table('chengyu')->inRandomOrder()->first();
-
-        $chengyu = new Chengyu();
-        $data = $chengyu->find(30906);
-        $data->view_count = 1;
-        $res = $data->save();
-        $chengyu::destroy();
-        dd($res);
-    }
-
-    public function callback()
-    {
-        echo 'success';
-    }
-
+    /**
+     * 保存天气
+     * @param Request $request
+     * @return \Illuminate\Database\Eloquent\Model|null|string
+     */
     public function getWeather(Request $request)
     {
         $data = $request->all();
-
         foreach ($data['tqInfo'] as $k => $v) {
             $v['city'] = $data['city'];
             $v['bWendu'] = (int)$v['bWendu'];
             $v['yWendu'] = (int)$v['yWendu'];
             $res = Model\Weather::firstOrCreate(['city' => $data['city'], 'ymd' => $v['ymd']], $v);
         }
-
         return $res;
     }
 
-
-    public function saveAddress(Request $request)
-    {
-        return Model\WeatherAddress::firstOrCreate($request->all());
-    }
-
+    /**
+     * 采集页面
+     * @return \Illuminate\Http\Response
+     */
     public function gatherWeather()
     {
         return response()->view('gatherWeather')->header('Content-Type','gbk');
     }
 
+    /**
+     * 下载天气
+     * @param Request $request
+     * @return \Maatwebsite\Excel\BinaryFileResponse|string|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
     public function downloadWeather(Request $request)
     {
         ini_set('memory_limit','500M');
--- resources/views/gatherWeather.blade.php
+++ resources/views/gatherWeather.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>test ws</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
